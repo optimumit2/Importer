114c656022e911b0b763869c125287a30de5e865
optimumit23 <optimumit2@gmailc.om>
2013-04-28 23:35:58 +0200

    Revert "using optimizations"
    This reverts commit f7d68ec92fc82c1453d790d18dd60c3b841b78fe.

--- WpfModern/LoginPage.xaml.cs
+++ WpfModern/LoginPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using MS.Internal.Xml.XPath;
 using WpfModern.Configuration;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace WpfModern
 {
